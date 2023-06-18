459665613906af14dc8e12a9b5b5bf8de812a11a
cinta <cintatafur@gmail.com>
2023-06-18 19:40:40 +0200

    📌 video12(TIpos basicos u conceptos generales)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import "./style.css";
-/* import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts' */
+/* import './topics/01-basic-types' */
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   Hola Mundo
 `;
 
-console.log('Hola Mundo');
+console.log("Hola Mundo");
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,15 @@
+const name: string = "Cinta";
+
+let hpPoints: number | "FULL" = 95;
+
+const isAlive: boolean = true;
+
+hpPoints = "FULL";
+
+console.log({
+  name,
+  hpPoints,
+  isAlive,
+});
+
+export {};
