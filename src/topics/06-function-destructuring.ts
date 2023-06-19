091537bebcbb666cede620dba34c87e3f857cc9a
cinta <cintatafur@gmail.com>
2023-06-19 11:11:22 +0200

    📌 Videos19y20(Desestrucuturacion de argumentos)

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,39 @@
+interface Producto {
+  descripcion: string;
+  precio: number;
+}
+
+const phone: Producto = {
+  descripcion: "Nokia",
+  precio: 125.0,
+};
+
+const tablet: Producto = {
+  descripcion: "iPad Air",
+  precio: 250.0,
+};
+
+interface TaxCalculadorOpcion {
+  tax: number;
+  productos: Producto[];
+}
+
+function taxCalculation(options: TaxCalculadorOpcion): [number, number] {
+  const { tax, productos } = options;
+  let total = 0;
+  productos.forEach(({ precio }) => {
+    total += precio;
+  });
+
+  return [total, total * tax];
+}
+const carroCompra = [phone, tablet];
+const tax = 0.15;
+
+const [total, taxResult] = taxCalculation({
+  productos: carroCompra,
+  tax: tax,
+});
+console.log("Total", total);
+console.log("tax", taxResult);
+export {};
