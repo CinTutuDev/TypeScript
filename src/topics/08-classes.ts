02cf47fe5cf0a14033458f107812d0fba9527110
cinta <cintatafur@gmail.com>
2023-06-19 16:48:18 +0200

    📌 Herencia(clases)

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,15 +1,30 @@
 export class Person {
-  constructor(public name: string, 
-              private address: string = "No Address") {}
+  constructor(public firstName: string,
+   public lastName: string,
+   private address: string = "No Address") {}
 }
 
-export class Hero extends Person {
- constructor( public alterEgo: string, public edad: number, public nombreReal: string  ){
-  super(nombreReal, 'Badayork');
- }
+/* export class Person {
+  constructor(
+    public alterEgo: string,
+    public edad: number,
+    public nombreReal: string
+  ) {
+    
+  }
+} */
+
+export class Hero {
+  constructor(
+    public alterEgo: string,
+    public edad: number,
+    public nombreReal: string,
+    public person: Person
+  ) {}
 }
 
-
-const iroman = new Hero("Pepita", 41, 'Capullita');
+const personita = new Person("Caracolita",'Fernandez', "Júpiter");
+const iroman = new Hero("Pepita", 41, "Capullita",personita);
 
 console.log({ iroman });
+console.log({ personita });
