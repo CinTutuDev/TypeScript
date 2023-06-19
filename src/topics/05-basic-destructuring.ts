45bbe232cdc0767b2861070fd30f9f027e251895
cinta <cintatafur@gmail.com>
2023-06-19 10:28:52 +0200

    📌 Video18(Desestructuracion de arreglos)

--- src/topics/05-basic-destructuring.ts
+++ src/topics/05-basic-destructuring.ts
@@ -1,3 +1,4 @@
+/* ------------------Desestructuracion de Objetos-------------------------- */
 interface AudioPlayer {
   audioVolume: number;
   songDuration: number;
@@ -28,12 +29,26 @@ const {
 } = audioPlayer;
 /* const {author} = detail; */
 
-console.table({ audioPlayer });
+/* console.table({ audioPlayer });
 console.log("Song:", song);
 console.log("Duration:", duration);
 console.log("Song:", anotherSong);
 console.log("Autor:", author);
 console.log("Detail:", audioPlayer.detail.author);
-
 console.log({ audioPlayer });
+ */
+
+/* --------------------------------------Desestructuracion de Arreglos----------------------- */
+  
+ const[p1, p2, p3 = 'Not found' ]: string[] = ['Goku', 'Vegeta',/*  ' Trunk' */];
+ /* const Trunk = dbz[3] ||'No hay personaje'; */
+
+ console.log('Personaje 3:', p3);
+console.log('Personaje 2:', p2);
+
+
+
+
+
+
 export {};
